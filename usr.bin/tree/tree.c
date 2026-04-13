2889c607a7e55d10028f120830d109020c050542
p4tt7 <patricia.conde.lorente@gmail.com>
2026-04-12 22:44:48 -0400

    add tree command (wish me luck)

---
+++ usr.bin/tree/tree.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <dirent.h>    
+#include <sys/stat.h>  
+#include <string.h> 
+
+void
+print(int depth, char *name, struct stat info)
+{
+    for(int i = 0; i<depth;i++)
+    {
+        printf("    ");
+    }
+
+    if(S_ISDIR(info.st_mode))
+    {
+        printf("%s/\n", name);
+    }
+    else
+    {
+        printf("%s\n", name);
+    }
+
+}
+
+
+void
+tree(char *path, int depth)
+{
+    DIR *handle = opendir(path);
+
+    if(handle == NULL)
+    {
+        return;
+    }
+
+    struct dirent *in;
+    struct stat info;
+
+    while((in = readdir(handle)) != NULL)
+    {
+        char new_path[1024];
+        snprintf(new_path, sizeof(new_path), "%s/%s", path, in->d_name);
+
+        stat(new_path, &info);
+
+        if(strcmp(in->d_name, ".") == 0 || strcmp(in->d_name, "..") == 0)
+        {
+            continue;
+        }
+        // avoids infinite recursion into its parent or itself.
+
+        print(in->d_name, depth+1, info);
+
+        if(S_ISDIR(info.st_mode))
+        {
+            tree(new_path, depth+1);
+        }
+    }
+
+    closedir(handle);
+}
+
+int
+main(int argc, char *argv[])
+{
+    char *path = (argc > 1) ? argv[1] : ".";
+    printf("%s\n", path);
+    tree(path, 0);
+    return 0;
+}
